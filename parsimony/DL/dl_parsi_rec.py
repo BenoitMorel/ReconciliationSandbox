08ce9c192f425ef8b3c14664a019f189e32521ec
BenoitMorel <benomorel@gmail.com>
2018-07-20 22:19:27 +0200

    Add my own tree structure and a conversion from/to the newick parser/dumper tree structure

--- parsimony/DL/dl_parsi_rec.py
+++ parsimony/DL/dl_parsi_rec.py
@@ -2,6 +2,48 @@ import sys
 sys.path.insert(0, '../../deps/python-newick/src/')
 import newick
 
+class PhyloNode(object):
+    def __init__(self, name=""):
+        self.dico = {}
+        if (name == None):
+            self.dico["name"] = ""
+        else:
+            self.dico["name"] = name
+        self.children = []
+
+    @staticmethod
+    def read(newick_filename):
+        return PhyloNode.build_from_newick_object(
+                newick.read(newick_filename)[0])
+    
+    @staticmethod
+    def build_from_newick_object(newick_node):
+        node = PhyloNode(newick_node.name)
+        for child in newick_node.descendants:
+            node.add_child(PhyloNode.build_from_newick_object(child))
+        return node
+  
+    def get_newick_node(self, label):
+        result = newick.Node(self.dico[label])
+        for child in self.children:
+            result.descendants.append(child.get_newick_node(label))
+        return result
+
+    def get_ascii(self, label):
+        newick_node = self.get_newick_node(label)
+        return newick_node.ascii_art()
+
+    def set_name(self, name):
+        self.dico["name"] = name
+
+    def get_name(self):
+        return self.dico["name"]
+    
+    def add_child(self, node):
+        self.children.append(node)
+
+    
+
 
 def load_mapping(mapping_file):
     mapping = {}
@@ -14,6 +56,11 @@ def load_mapping(mapping_file):
             mapping[split[0]] = split[1]
     return mapping
 
+def map_gene_to_species(gene_tree, species_tree, mapping):
+    """
+    Recursively map the gene nodes to species tree nodes
+    """
+
 if (len(sys.argv) != 4):
     print("Syntax: python dl_parsi_rec.py genes species map")
     sys.exit(1)
@@ -22,11 +69,10 @@ species_newick = sys.argv[1]
 genes_newick = sys.argv[2]
 mapping_file = sys.argv[3]
 
-species_tree = newick.read(species_newick)
-print("Species tree: " + newick.dumps(species_tree))
-gene_tree = newick.read(genes_newick)
-print("Gene tree: " + newick.dumps(gene_tree))
+species_tree = PhyloNode.read(species_newick)
+print("Species tree: \n" + species_tree.get_ascii("name"))
+gene_tree = PhyloNode.read(genes_newick)
+print("Gene tree: \n" + gene_tree.get_ascii("name"))
 mapping = load_mapping(mapping_file)
 print("Mapping: " + str(mapping))
-
 
