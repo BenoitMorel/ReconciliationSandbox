e0ba3a3a938c1aaf00a55a32e2d32c8c47b27b12
BenoitMorel <benomorel@gmail.com>
2018-07-20 21:33:55 +0200

    Add a simple dataset with a gene tree, a species tree and a mapping file

--- parsimony/DL/dl_parsi_rec.py
+++ parsimony/DL/dl_parsi_rec.py
@@ -1,5 +1,32 @@
 import sys
+sys.path.insert(0, '../../deps/python-newick/src/')
+import newick
 
-print("Todo")
+
+def load_mapping(mapping_file):
+    mapping = {}
+    lines = open(mapping_file).readlines()
+    for line in lines:
+        split = line.split(" ")
+        if (split[1].endswith("\n")):
+            mapping[split[0]] = split[1][:-1]
+        else:
+            mapping[split[0]] = split[1]
+    return mapping
+
+if (len(sys.argv) != 4):
+    print("Syntax: python dl_parsi_rec.py genes species map")
+    sys.exit(1)
+
+species_newick = sys.argv[1]
+genes_newick = sys.argv[2]
+mapping_file = sys.argv[3]
+
+species_tree = newick.read(species_newick)
+print("Species tree: " + newick.dumps(species_tree))
+gene_tree = newick.read(genes_newick)
+print("Gene tree: " + newick.dumps(gene_tree))
+mapping = load_mapping(mapping_file)
+print("Mapping: " + str(mapping))
 
 
